ee3e56e03277a03392e3a9af40dd26df50b5773a
Rey Alvarez <nacho.rei@gmail.com>
2022-04-18 20:08:54 -0300

    Registrar Evento DOM

--- JS/main.js
+++ JS/main.js
@@ -9,16 +9,17 @@ const nombreUsuario = prompt("Bienvenidx a Gea BioCosmetica: ");
 titulo.innerText = "Hola " + nombreUsuario + "!!!"
 
 alert(titulo.innerText);
+
 //Modal
 
-const open = document.getElementById('open');
-const modal_container = document.getElementById('modal_container');
-const close = document.getElementById('close');
+const open = document.getElementById("open");
+const modal_container = document.getElementById("modal_container");
+const close = document.getElementById("close");
 
-open.addEventListener('click', () => {
-  modal_container.classList.add('show');
+open.addEventListener("click", () => {
+  modal_container.classList.add("show");
 });
 
-close.addEventListener('click', () => {
-  modal_container.classList.remove('show');
+close.addEventListener("click", () => {
+  modal_container.classList.remove("show");
 });
